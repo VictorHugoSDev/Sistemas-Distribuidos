d4d39195932c250c055399913bf943638b39b32d
VictorHugoSDev <victorhhugo@hotmail.com>
2025-10-23 20:22:43 -0300

    correcao do websocketeventlistener

--- Lista-de-Exercicios-2/websockets_exemplo/src/main/java/br/edu/iftm/tspi/sd/websockets_exemplo/handler/WebSocketEventListener.java
+++ Lista-de-Exercicios-2/websockets_exemplo/src/main/java/br/edu/iftm/tspi/sd/websockets_exemplo/handler/WebSocketEventListener.java
@@ -5,7 +5,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent; // IMPORT CORRETO
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.time.Instant;
 import java.util.Collections;
